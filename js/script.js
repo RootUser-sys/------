043e448e3f6df9499517b200a72971636b816917
Root <82734309+RootUser-sys@users.noreply.github.com>
2022-05-12 17:16:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-let answer = prompt('Введите сюда Ваш рост');
-document.write('Ваш рост: ' + '139см')
+/* const numberOfFilms = prompt('Count', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+
+console.log(personalMovieDB)
+document.write('Количество фильмов:' + numberOfFilms + 'Название последнего фильма:' + a + 'Оценка:' + b); */
+
+
+let password = prompt('Введите пароль', '');
+if (password == 333) {
+    document.write('Вы вошли');
+} else {
+    document.write('Вы лошок');
+}
